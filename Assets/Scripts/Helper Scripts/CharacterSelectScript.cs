7ace6aa6b5b6a61cafc5f63acfb8750343ee497f
Thyvene <karim.el-karroumy@epitech.eu>
2020-08-10 12:36:11 +0200

    Implement Main menu controller script and Characters selection script (WIP in Game Menu)

---
+++ Assets/Scripts/Helper Scripts/CharacterSelectScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectScript : MonoBehaviour
+{
+    public GameObject[] available_Heroes;
+
+    private int currentIndex;
+
+    public Text selected_Text;
+    public GameObject starIcon;
+    public Image selectBtn_Image;
+    public Sprite button_Green;
+    public Sprite button__Blue;
+
+    private bool[] heroes;
+
+    public Text starScoreText;
+
+    private void Start()
+    {
+        InitializeCharacters();
+    }
+
+    void InitializeCharacters()
+    {
+        currentIndex = 0;
+
+        for (int i = 0; i < available_Heroes.Length; i++)
+            available_Heroes[i].SetActive(false);
+
+        available_Heroes[currentIndex].SetActive(true);
+
+        // TODO Call character change via GameManager script
+    }
+
+    public void NextHero()
+    {
+        available_Heroes[currentIndex].SetActive(false);
+
+        if (currentIndex + 1 == available_Heroes.Length)
+            currentIndex = 0;
+        else
+            currentIndex++;
+
+        available_Heroes[currentIndex].SetActive(true);
+    }
+
+    public void PreviousHero()
+    {
+        available_Heroes[currentIndex].SetActive(false);
+
+        if (currentIndex - 1 == -1)
+            currentIndex = available_Heroes.Length - 1;
+        else
+            currentIndex--;
+
+        available_Heroes[currentIndex].SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Helper Scripts/MainMenuController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject heroMenu;
+    public Text starScoreText;
+
+    public Image music_Img;
+    public Sprite music_Off;
+    public Sprite music_On;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void HeroMenu()
+    {
+        heroMenu.SetActive(true);
+        // TODO Display the star score
+    }
+
+    public void HomeButton()
+    {
+        heroMenu.SetActive(false);
+    }
+}
